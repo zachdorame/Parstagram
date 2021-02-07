51f406a75c48e23c4702fb046991c2b95b25103c
zach dorame barajas <zachdoramebarajas@gmail.com>
2021-02-06 18:38:26 -0800

    Photos can be stored and read from database

---
+++ Parstagram/PostCell.swift
@@ -0,0 +1,27 @@
+//
+//  PostCell.swift
+//  Parstagram
+//
+//  Created by Zach Dorame-Barajas on 2/6/21.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var captionLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var photoView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
